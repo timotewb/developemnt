b0dccde3babd2bfa2adfd3daff680313e7f81398
timotewb <45491056+timotewb@users.noreply.github.com>
2022-09-18 14:31:54 +1200

    surrealdb

--- go/1.19/rss/main.go
+++ go/1.19/rss/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
+var deBug bool = false
+
+// var getURL string = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+var getURL string = "https://www.rnz.co.nz/rss/business.xml"
+
+// input struct(s)
 type DataFormat struct {
 	XMLName xml.Name `xml:"rss"`
 	Rss     Channel  `xml:"channel"`
 }
 type Channel struct {
-	XMLName xml.Name `xml:"channel"`
-	Items   []Item   `xml:"item"`
+	// XMLName xml.Name `xml:"channel"`
+	Items []Item `xml:"item"`
 }
 type Item struct {
-	XMLName     xml.Name     `xml:"item"`
+	// XMLName     xml.Name     `xml:"item"`
 	Title       string       `xml:"title"`
 	Link        string       `xml:"link"`
 	Description string       `xml:"description"`
@@ -27,8 +35,22 @@ type Item struct {
 	ImageCredit string       `xml:"credit"`
 }
 type ImageURLAttr struct {
-	XMLName xml.Name `xml:"content"`
-	Url     string   `xml:"url,attr"`
+	// XMLName xml.Name `xml:"content"`
+	Url string `xml:"url,attr"`
+}
+
+// output struct
+type DataFormatOut struct {
+	Items []ItemsOut
+}
+type ItemsOut struct {
+	Title       string
+	Link        string
+	Description string
+	Creator     string
+	PubDate     string
+	ImageURL    string
+	ImageCredit string
 }
 
 // tweaked from: https://stackoverflow.com/a/42718113/1170664
@@ -52,15 +74,50 @@ func getXML(url string) ([]byte, error) {
 }
 
 func main() {
-	if xmlBytes, err := getXML("https://rss.nytimes.com/services/xml/rss/nyt/World.xml"); err != nil {
+	if xmlBytes, err := getXML(getURL); err != nil {
 		log.Printf("Failed to get XML: %v", err)
 	} else {
-		//fmt.Print(string(xmlBytes))
+		// remove any difficult strings
+		xmlBytes = []byte(strings.ReplaceAll(string(xmlBytes), "atom:link", "atomlink"))
+
 		var result DataFormat
 		xml.Unmarshal(xmlBytes, &result)
 		if err := xml.Unmarshal(xmlBytes, &result); err != nil {
 			log.Fatal(err)
+		} else {
+			if deBug {
+				for i, s := range result.Rss.Items {
+					fmt.Println(i)
+					fmt.Printf("Title:       %v\n", s.Title)
+					fmt.Printf("Link:        %v\n", s.Link)
+					fmt.Printf("Description: %v\n", strings.TrimSpace(s.Description))
+					fmt.Printf("Creator:     %v\n", s.Creator)
+					fmt.Printf("PubDate:     %v\n", s.PubDate)
+					fmt.Printf("ImageURL:    %v\n", s.ImageURL)
+					fmt.Printf("ImageCredit: %v\n", s.ImageCredit)
+					fmt.Println()
+				}
+			}
+
+			// tidy up and output to file as an array of structs (list of json)
+			var outData []ItemsOut
+			for _, d := range result.Rss.Items {
+				var o ItemsOut
+				o.Creator = strings.TrimSpace(d.Creator)
+				o.Description = strings.TrimSpace(d.Description)
+				o.ImageCredit = strings.TrimSpace(d.ImageCredit)
+				o.ImageURL = strings.TrimSpace(d.ImageURL.Url)
+				o.Link = strings.TrimSpace(d.Link)
+				o.PubDate = strings.TrimSpace(d.PubDate)
+				o.Title = strings.TrimSpace(d.Title)
+				outData = append(outData, o)
+			}
+			b, err := json.MarshalIndent(outData, "", " ")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			_ = ioutil.WriteFile("test.json", b, 0777)
 		}
-		fmt.Print(result.Rss.Items)
 	}
 }
