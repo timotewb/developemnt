209a6706d058de01be16a37b5fc801c693b11166
timotewb <timote.wb@gmail.com>
2022-11-19 21:06:25 +1300

    ssh reader

---
+++ go/1.19/go-ssh-return/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
+)
+
+func main() {
+	// ssh config
+	hostKeyCallback, err := knownhosts.New("/home/timotewb/.ssh/known_hosts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := &ssh.ClientConfig{
+		User: "sysadmin",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("rX#=TZ4V"),
+		},
+		HostKeyCallback: hostKeyCallback,
+	}
+	// connect ot ssh server
+	conn, err := ssh.Dial("tcp", "192.168.144.211:22", config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var buff bytes.Buffer
+	session.Stdout = &buff
+	if err := session.Run("systemctl status api.service --lines 5"); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(buff.String())
+
+	defer session.Close()
+	defer conn.Close()
+}
