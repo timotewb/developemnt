446dd3e5c12aab2a1061a11d5bb18e328ad336d5
timotewb <timote.wb@gmail.com>
2022-11-21 21:34:16 +1300

    ssh return cont

--- go/1.19/go-ssh-return/main.go
+++ go/1.19/go-ssh-return/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"strings"
 
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/knownhosts"
@@ -38,7 +39,15 @@ func main() {
 	if err := session.Run("systemctl status api.service --lines 5"); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(buff.String())
+	var s string = buff.String()
+	s1 := strings.Split(s, "\n")
+	for _, s2 := range s1 {
+		if strings.Contains(s2, "└─") || strings.Contains(s2, "├─") {
+			fmt.Println("yes - ", s2)
+		} else {
+			fmt.Println(s2)
+		}
+	}
 
 	defer session.Close()
 	defer conn.Close()
